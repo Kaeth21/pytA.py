3a80f243daa48bef4bb4392ca00779842c91f5a5
Kamil sedik <87916174+Kaeth21@users.noreply.github.com>
2023-09-08 23:00:34 +0200

    taskA

---
+++ pythA.py
@@ -0,0 +1,11 @@
+### Uppgift 1
+
+Vi brukar säga att det finns statiskt typade språk och dynamiskt typade språk.
+Python är ett dynamiskt typat språk som har en "interpreter", dvs. koden vi skriver tolkas av ett program som omvandlar det till binära tal. Vad innebär det i praktiken för oss när vi skriver kod, dvs. att python är dynamiskt typat?
+
+Svara med kod och kommentarer.
+
+#### Response
+In Python, being dynamically typed means that you don't have to declare the data type of a variable. You can change the type of a variable during runtime. For example, you can initially assign an integer value to a variable and later assign a string value to the same variable without any issues. This flexibility can be useful, but it's important to handle it carefully to avoid unexpected behavior.
+
+#### Example Code
